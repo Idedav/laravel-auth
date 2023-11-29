62d4d36d91c968d6ed332a30ea965bdb350ab697
Idedav <xdavide.correa@gmail.com>
2023-11-29 16:20:12 +0100

    [add] active class

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,9 +1,21 @@
 <aside class="bg-dark">
     <nav>
         <ul>
-            <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Dashboard</a></li>
-            <li><a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-folder"></i> Projects</a></li>
-            <li><a href="{{ route('admin.tecnologies.index') }}"><i class="fa-solid fa-code"></i> Tecnologies</a></li>
+            <li><a href="{{ route('admin.home') }}"
+                    class="{{ Route::currentRouteName() === 'admin.home' ? 'active' : '' }}">
+                    <i class="fa-solid fa-house"></i> Dashboard
+                </a>
+            </li>
+            <li><a href="{{ route('admin.projects.index') }}"
+                    class="{{ Route::currentRouteName() === 'admin.projects.index' ? 'active' : '' }}">
+                    <i class="fa-solid fa-folder"></i> Projects
+                </a>
+            </li>
+            <li><a href="{{ route('admin.tecnologies.index') }}"
+                    class="{{ Route::currentRouteName() === 'admin.tecnologies.index' ? 'active' : '' }}">
+                    <i class="fa-solid fa-code"></i> Tecnologies
+                </a>
+            </li>
         </ul>
     </nav>
 </aside>
